e35e20bcdb05d281f12386e5787afe3d7cd301ee
Jonathan Arnett <jonarnett90@gmail.com>
2022-06-15 13:44:33 -0400

    Add fetch/2 to Repo

--- lib/chatter/repo.ex
+++ lib/chatter/repo.ex
@@ -2,4 +2,15 @@ defmodule Chatter.Repo do
   use Ecto.Repo,
     otp_app: :chatter,
     adapter: Ecto.Adapters.Postgres
+
+  @type id :: any
+  @type model :: module
+
+  @spec fetch(model, id) :: {:ok, struct} | :error
+  def fetch(model, id) do
+    case get(model, id) do
+      nil -> :error
+      struct -> {:ok, struct}
+    end
+  end
 end
